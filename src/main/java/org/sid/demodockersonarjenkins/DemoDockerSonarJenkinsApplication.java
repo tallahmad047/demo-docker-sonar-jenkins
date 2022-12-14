af4dd15c3e8f821b877c87fcc8366d1c9f3d09b9
tallahmad047 <tallahmad047@gmail.com>
2022-12-14 20:47:12 +0000

    test 1

---
+++ src/main/java/org/sid/demodockersonarjenkins/DemoDockerSonarJenkinsApplication.java
@@ -0,0 +1,13 @@
+package org.sid.demodockersonarjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDockerSonarJenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoDockerSonarJenkinsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sid/demodockersonarjenkins/DemoDockerSonarJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.demodockersonarjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDockerSonarJenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
